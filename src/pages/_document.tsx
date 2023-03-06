8c618824064ed00b84e489cf0b3b20feeefbbf18
Jonatas <jonatasgs7@gmail.com>
2023-03-06 15:30:32 -0300

    Inserção do Google Tag Manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { TagManager } from '@/scripts/TagManager'
 
 export default function Document() {
   return (
     <Html lang="pt-br">
       <Head />
+      <TagManager />
       <body>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PLZKHBQ"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
       </body>
     </Html>
   )
